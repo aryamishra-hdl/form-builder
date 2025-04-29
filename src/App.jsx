ceae42e3f3fd6b5346ee7ee456949564c0b8737f
Arya Mishra <arya.mishra@hakuhododata-labs.com>
2025-04-29 18:26:53 +0530

    fixed problem with version

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,12 @@ import './App.css'
 import InputBuilder from './Pages/InputBuilder'
 import InputBuilder1 from './Pages/InputBuilder1'
 import LoginPage from './Pages/LoginPage'
-import RegionMap from './Pages/RegionMap'
 
 function App() {
 
   return (
     <div>
       {/* <LoginPage/> */}
-      {/* <RegionMap/> */}
       {/* <InputBuilder/> */}
       <InputBuilder1/>
     </div>
